9ba01e7a3aa9e8504faef642cab76f91fe547d94
arika <delete0093@gmail.com>
2022-04-22 12:59:31 +0000

    fix port

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import express, {Request, Response} from "express"
 
 export function launchServer(){
 	const app = express();
+	const port = process.env?.PORT || 80
 	app.get("/", (req: Request, res: Response) => {
 		res.status(200).send("Express!!");
 	});
-	app.listen(80);
+	app.listen(port);
 	console.log("server launched...")
 }
